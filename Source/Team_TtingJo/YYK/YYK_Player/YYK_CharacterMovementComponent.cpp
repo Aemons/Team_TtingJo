1222066ac461ecbdb3392442ccaeb5b280ca623c
YOOKYUNG <thelunartears@gmail.com>
2024-11-30 12:45:59 +0900

    capsule trace 생성

--- Source/Team_TtingJo/YYK/YYK_Player/YYK_CharacterMovementComponent.cpp
+++ Source/Team_TtingJo/YYK/YYK_Player/YYK_CharacterMovementComponent.cpp
@@ -2,4 +2,38 @@
 
 
 #include "YYK_CharacterMovementComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
 
+void UYYK_CharacterMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	TraceClimbableSurfaces();
+}
+
+
+#pragma region ClimbTraces
+TArray<FHitResult> UYYK_CharacterMovementComponent::DoCapsuleTraceMultiByObject(const FVector& _start,
+	const FVector& _end, bool bShowDebugShape)
+{
+	TArray<FHitResult> OutCapsuleTraceHitResults;
+	
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(this, _start, _end, ClimbCapsuleTraceRadius, ClimbCapsuleTraceHalfHeight
+		,ClimbableSurfaceTraceTypes, false, TArray<AActor*>(),
+		bShowDebugShape? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutCapsuleTraceHitResults,
+		false
+		);
+	return OutCapsuleTraceHitResults;
+}
+#pragma endregion
+
+#pragma region ClimbCore
+void UYYK_CharacterMovementComponent::TraceClimbableSurfaces()
+{
+	const FVector startOffset=UpdatedComponent->GetForwardVector()*30.f;
+	const FVector start=UpdatedComponent->GetComponentLocation()+startOffset;
+	const FVector end=start+UpdatedComponent->GetForwardVector();
+	DoCapsuleTraceMultiByObject(start,end,true);
+}
+#pragma endregion
--- Source/Team_TtingJo/YYK/YYK_Player/YYK_CharacterMovementComponent.h
+++ Source/Team_TtingJo/YYK/YYK_Player/YYK_CharacterMovementComponent.h
@@ -13,5 +13,29 @@ UCLASS()
 class TEAM_TTINGJO_API UYYK_CharacterMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
+
+private:
+#pragma region ClimbTrace
+	TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& _start, const FVector& _end, bool bShowDebugShape=false);
+#pragma endregion
+
+#pragma region ClimbCore
+	void TraceClimbableSurfaces();
+
+#pragma endregion
+
+public:
+#pragma region ClimbVariables
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Character Movement Climbing")
+	float ClimbCapsuleTraceRadius=50.f;
 	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Character Movement Climbing")
+	float ClimbCapsuleTraceHalfHeight=72.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Character Movement Climbing")
+	TArray<TEnumAsByte<EObjectTypeQuery>> ClimbableSurfaceTraceTypes;
+#pragma endregion
+
+public:
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
 };
